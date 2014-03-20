4ef3ad40fd113c81607529971882bd5731fc83ef
AnthraX1 <oneshootonekill@gmail.com>
2014-03-20 23:48:26 +1100

    Update gmworker.py

--- gmworker.py
+++ gmworker.py
@@ -4,82 +4,24 @@ import zlib
 import socket
 import urllib,urllib2
 import re
-import threading
 import sys
 import chardet
 import time
 import json
-from threading import Thread
-from Queue import Queue
+from multiprocessing import Process, JoinableQueue as Queue,Lock,Manager
 
 
-THREADS = 20
-TIMEOUT=5
+THREADS = 100
+TIMEOUT=2
 GMSERVER=['']
 REPORTURL=''
 KEY=''
-RESULT={}
-lock = threading.Lock()
 
-#For python2.4 Queue class...
-if sys.version_info[1]==4:
-	class Queue(Queue):
-		def __init__(self, maxsize=0):
-			self.maxsize = maxsize
-			self._init(maxsize)
-			self.mutex = threading.Lock()
-			self.not_empty = threading.Condition(self.mutex)
-			self.not_full = threading.Condition(self.mutex)
-			self.all_tasks_done = threading.Condition(self.mutex)
-			self.unfinished_tasks = 0
-
-		def task_done(self):
-			self.all_tasks_done.acquire()
-			try:
-				unfinished = self.unfinished_tasks - 1
-				if unfinished <= 0:
-					if unfinished < 0:
-						raise ValueError('task_done() called too many times')
-					self.all_tasks_done.notifyAll()
-				self.unfinished_tasks = unfinished
-			finally:
-				self.all_tasks_done.release()
-
-		def join(self):
-			self.all_tasks_done.acquire()
-			try:
-				while self.unfinished_tasks:
-					self.all_tasks_done.wait()
-			finally:
-				self.all_tasks_done.release()
-		def put(self, item, block=True, timeout=None):
-			self.not_full.acquire()
-			try:
-				if self.maxsize > 0:
-					if not block:
-						if self._qsize() == self.maxsize:
-							raise Full
-					elif timeout is None:
-						while self._qsize() == self.maxsize:
-							self.not_full.wait()
-					elif timeout < 0:
-						raise ValueError("'timeout' must be a positive number")
-					else:
-						endtime = _time() + timeout
-						while self._qsize() == self.maxsize:
-							remaining = endtime - _time()
-							if remaining <= 0.0:
-								raise Full
-							self.not_full.wait(remaining)
-				self._put(item)
-				self.unfinished_tasks += 1
-				self.not_empty.notify()
-			finally:
-				self.not_full.release()	
-
-
-
-
+mgr=Manager()
+RESULT=mgr.dict()
+ERROR=mgr.dict()
+lock = Lock()
+lockerr=Lock()
 def processlist(data):
 	res=''
 	try:
@@ -102,68 +44,77 @@ def worker_scan(worker,job):
 	if dlist==False:
 		print 'No list'
 		return 'Error'
-	for i in xrange(THREADS):
-			t = Thread(target=scanner)
-			t.daemon=True
-			t.start()	
 	for item in dlist:
 		if item=='' or item==None:
 			continue
 		q.put(item)
-	q.join()
+		#print 'put item'
+	while q.qsize()>THREADS/2:
+		time.sleep(0.5)
+		#print 'Sleeping...'
+	#q.join()
 	return 'Done'
 
 def autosend():
 	while True:
-		#print 'wait...'
-		if len(RESULT)>0 and q.empty():
-			lock.acquire()
+		#print RESULT
+		print 'wait...'
+		if len(RESULT)>0:
 			processresult()
-			lock.release()
-		time.sleep(30)# send back scanned data every 30s
+		if len(ERROR)>0:
+			processerr()	
+		time.sleep(10)# send back scanned data every 10s
 
 def processresult():
 	if len(RESULT)==0:
 		return False
-	data=base64.b64encode(zlib.compress(json.dumps(RESULT)))
+	print 'data len:',len(RESULT)
+	lock.acquire()
+	res=dict(RESULT)
+	RESULT.clear()
+	lock.release()
+	data=base64.b64encode(zlib.compress(json.dumps(res)))
 	stat=postback({"type":"data","data":data})
-	if stat!=False:
-		RESULT.clear()
+	print 'sent data'
 
+def processerr():
+	if len(ERROR)==0:
+		return False
+	print 'err len:',len(ERROR)
+	lockerr.acquire()
+	res=dict(ERROR)
+	ERROR.clear()
+	lockerr.release()
+	data=base64.b64encode(zlib.compress(json.dumps(res)))
+	stat=postback({"type":"error","data":data})
+	print 'sent err'
 
 def scanner():
 	while True:
 		domain=q.get()
-		if len(domain)<3:
-			q.task_done()
-			continue
 		url='http://'+domain
-		#print url
 		httpheaders={"Content-type": "application/x-www-form-urlencoded","Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8","User-Agent":"Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/20.0.11"}
 		req=urllib2.Request(url,headers=httpheaders)
 		try:
-			ip=getip(domain)
-			r=urllib2.urlopen(req,timeout=TIMEOUT)
+			r=urllib2.urlopen(req,None,TIMEOUT)
 			content=r.read()
 			headers=getheader(r)
 			title=gettitle(content)
+			ip=getip(domain)
 			data={}
 			#data['domain']=domain
 			data['ip']=json.dumps(ip)
 			data['title']=title
 			data['header']=headers
+			lock.acquire()
 			RESULT[domain]=data
+			lock.release()
 			#print RESULT
 			#print content #debug only
 		except Exception as e:
-			errdata=base64.b64encode(zlib.compress(json.dumps({"domain":domain,"exception":str(e)})))
-			errmsg={"type":"error","data":errdata}
-			#print errmsg
-			postback(errmsg)
-		if len(RESULT)>=THREADS:
-			lock.acquire()
-			processresult()
-			lock.release()
+			lockerr.acquire()
+			ERROR[domain]=str(e)
+			lockerr.release()
 		q.task_done()
 
 
@@ -175,7 +126,7 @@ def getheader(handle):
 
 def gettitle(content):
 	#print content
-	tt=re.search("<.*>(.*?)</title>",content,re.IGNORECASE|re.DOTALL)
+	tt=re.search("<title.*?>(.*?)</title>",content,re.IGNORECASE|re.DOTALL)
 	try:
 		title=tt.group(1)
 	except:
@@ -187,7 +138,7 @@ def gettitle(content):
 	except:
 		pass
 	#print charset
-	if charset==None:
+	if charset==None or charset=='':
 		charset=chardet.detect(title).get('encoding','utf-8')
 	if charset!='utf-8' or charset!='utf8':
 		title=title.decode(charset,'ignore').encode('utf-8')
@@ -210,12 +161,12 @@ def postback(postdata):#postdata should be dict
 	try:
 		r=urllib2.urlopen(req)
 		reply=r.read()
-		#print reply
-		if reply!='success':
-			print reply
+		print 'reply:',reply
+#		if reply!='success':
+			#print reply
 		return True	
 	except Exception as e:
-		print str(e)
+		print e
 		return False
 
 
@@ -223,12 +174,18 @@ def postback(postdata):#postdata should be dict
 if __name__ == "__main__":
 	worker=gm.GearmanWorker(GMSERVER)
 	q=Queue()
+	for i in xrange(THREADS):
+			t = Process(target=scanner)
+			t.daemon=True
+			t.start()	
+	print 'Threads started'
 	worker.register_task("scan1", worker_scan)
 	print 'Worker started'
-	ts = Thread(target=autosend)
+	ts = Process(target=autosend)
 	ts.daemon=True
 	ts.start()
 	print 'Background thread started'
 	worker.work()
+
 
 
